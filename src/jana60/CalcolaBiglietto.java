a5386608ac6149ce67e13a8d74d89aa9e4f3e4fb
LuigiZannella <zannellaluigi@outlook.com>
2022-06-07 16:31:25 +0200

    fine esercizio

--- src/jana60/CalcolaBiglietto.java
+++ src/jana60/CalcolaBiglietto.java
@@ -1,10 +1,40 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Variabili
+		float numeroChilometri;
+		int anniPasseggero;
+		float scontoMinorenni;
+		float scontoOver65;
+		// Scanner
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Chilometri da percorrere");
+		numeroChilometri = scan.nextFloat();
+		
+		System.out.println("Età passeggero");
+		anniPasseggero = scan.nextInt();
+		//variabili prezzi biglietti
+		float prezzoBiglietto = numeroChilometri * 0.21f;
+			//calcolo prezzo biglietto
+		if(anniPasseggero < 18) {
+			scontoMinorenni = prezzoBiglietto - (prezzoBiglietto * 0.2f);
+			System.out.println("Normalmente il prezzo del biglietto sarebbe:" + prezzoBiglietto + " euro");
+			System.out.println("Ma per i minorenni verrà applicato uno sconto del 20%. Quindi il prezzo del biglietto scontato è di: ");
+			System.out.println(scontoMinorenni + "euro");
+		} else if (anniPasseggero > 65) {
+			scontoOver65 = prezzoBiglietto - (prezzoBiglietto * 0.4f);
+			System.out.println("Normalmente il prezzo del biglietto sarebbe:" + prezzoBiglietto + " euro");
+			System.out.println("Ma per gli over65 verrà applicato uno sconto del 40%. Quindi il prezzo del biglietto scontato è di: ");
+			System.out.println(scontoOver65 + "euro");
+		}else {
+			System.out.println("Il prezzo del biglietto è: " + prezzoBiglietto);
+		}
 
+		scan.close();
 	}
 
 }
